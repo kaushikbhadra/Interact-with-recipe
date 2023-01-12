fe078c5c8c6f80fe41e5ce5d302fc8b123990294
Kaushik Bhadra <kbhadra100@gmail.com>
2023-01-12 17:16:07 +0530

    initial add recipe component items and details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe("panner tikka masala", "made with cow milk product.", "https://live.staticflickr.com/7400/10213777266_d62f554fd0_b.jpg"),
+    new Recipe("panner tikka masala", "made with cow milk product.", "https://live.staticflickr.com/7400/10213777266_d62f554fd0_b.jpg"),
+    new Recipe("panner tikka masala", "made with cow milk product.", "https://live.staticflickr.com/7400/10213777266_d62f554fd0_b.jpg"),
+    new Recipe("panner tikka masala", "made with cow milk product.", "https://live.staticflickr.com/7400/10213777266_d62f554fd0_b.jpg"),
+    new Recipe("panner tikka masala", "made with cow milk product.", "https://live.staticflickr.com/7400/10213777266_d62f554fd0_b.jpg")
+  ];
 }
+ 
