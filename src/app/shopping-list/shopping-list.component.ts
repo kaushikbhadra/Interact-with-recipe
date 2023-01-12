142aa2ab380803eb96c9576cab72a71b765fe57b
Kaushik Bhadra <kbhadra100@gmail.com>
2023-01-12 18:23:11 +0530

    add initial setup for shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Ingerdient } from '../shared/ingredient-model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingerdient[] = [
+    new Ingerdient("panner", 250),
+    new Ingerdient("haldi", 5),
+  ];
 }
