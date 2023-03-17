e9f5867be6ca6efd0e3a94ef2c8a4a5193d2c35d
Kaushik Bhadra <kbhadra100@gmail.com>
2023-03-17 15:29:55 +0530

    adding lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)}
 ];
 
 @NgModule({
--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -9,7 +9,7 @@ import { RecipeDetailsComponent } from './recipe-details/recipe-details.componen
 import { RecipesResolverService } from './recipes-resolver.service';
 
 const routes: Routes = [
-  {path: 'recipes', component: RecipesComponent,
+  {path: '', component: RecipesComponent,
      canActivate: [AuthGuard],   
      children: [
         {path: '', component: RecipeStartComponent},
